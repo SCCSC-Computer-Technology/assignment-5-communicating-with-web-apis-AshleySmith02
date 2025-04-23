ee25afede0163cc645e692f1df8a046d9da7b6c9
AshleySmith02 <asmith45@sccsc.edu>
2025-04-22 21:49:19 -0400

    Add files via upload

---
+++ ASmith-CPT-206-Lab-5/Controllers/StatesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASmith_CPT_206_Lab_5.Controllers
+{
+    public class StatesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
